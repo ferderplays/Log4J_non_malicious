8bbfc874a6463bd0388426db55d101ed735d7bfd
FERDdeveloper <86313430+ferderplays@users.noreply.github.com>
2021-12-29 14:42:36 +0100

    1.0

---
+++ Uwu.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Uwu {
+    public static void main(String[] args) {
+        try {
+            Runtime.getRuntime().exec(
+                    "cmd.exe /c echo Update-uj si Javu pls, prtž jsem právě použil RCE na tvém počítači, přes jednu správu v chatu, nemám tvoje hesla, ale někdo jiný může použít podobný Exploit a získat tvoje hesla a IP");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
